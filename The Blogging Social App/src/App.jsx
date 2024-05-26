e98cefa18c279df58a573ddda329a826ea055b5f
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-05-26 20:34:06 +0300

    hit

--- The Blogging Social App/src/App.jsx
+++ The Blogging Social App/src/App.jsx
@@ -3,9 +3,27 @@ import Home from './Pages/home/home'
 import Profile from './Pages/profile/Profile'
 import Login from './Pages/login/Login'
 import Register from './Pages/register/Register'
+import {
+    BrowserRouter
+    ,Route,
+    Routes
+  } from "react-router-dom";
+
 
 function App() {
-    return <Home />
-}
-
-export default App
+    return (
+      <>
+        <BrowserRouter basename='/The-Blogging-Social-App-main'> 
+          <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/Profile' element={<Profile/>}/>
+              <Route path='/register' element={<Register/>}/>
+           </Routes>
+          <Footer/> 
+        </BrowserRouter> 
+        
+      </>
+    );
+  }
+export default App;
--- The Blogging Social App/src/main.jsx
+++ The Blogging Social App/src/main.jsx
@@ -1,30 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App.jsx'
-import {
-  BrowserRouter,
-  Router,Route,
-  Routes
-} from "react-router-dom";
-import './index.css'
-import Login from './Pages/login/Login.jsx';
-import Profile from './Pages/profile/Profile.jsx';
-import Register from './Pages/register/Register.jsx';
 
-
-function Main() {
-  return (
-    <>
-      <BrowserRouter basename='/The-Blogging-Social-App-main'> 
-        <Routes>
-            <Route exact path='/' element={<App/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/Profile' element={<Profile/>}/>
-            <Route path='/register' element={<Register/>}/>
-         </Routes>
-        <Footer/> 
-      </BrowserRouter> 
-      
-    </>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
